3771ae9aba3d032851c4eed0a2807e8f8bbc6e3d
Mirco Michel <mirco.michel@scilifelab.se>
2014-03-22 20:05:35 +0100

    update folding stepw

--- folding/rosetta/fold.py
+++ folding/rosetta/fold.py
@@ -6,39 +6,43 @@ from subprocess import Popen
 
 from localconfig import *
 
-### NOTE: Rosetta itself does not run in parallel, it uses a single core per run.
-### Since it is non-deterministic (based on markov chain monte carlo) one can just
-### run multiple instances at the same time and distribute them onto multiple cores.
+### default value
+n_cores = 16
+n_decoys = 2000
 
-#rootdir = '/home/x_mirmi/pcons-fold/folding/rosetta/test/rosetta'
-#rosettadir = '/home/x_mirmi/glob/rosetta'
-
-### default value for one triolith node
-cores = 16
-
-### change it if other value given
-if sys.argv[1] == '-c':
+### parse parameters
+if '-c' in sys.argv:
+    idx = sys.argv.index('-c')    
     try:
-        cores = int(sys.argv[2])
+        n_cores = int(sys.argv[idx+1])
     except:
-        print 'Number of cores -c must be an integer number, %r is not. Default is %s.' % (sys.argv[2], cores)
+        print 'Number of cores -c must be an integer, %r is not. Default is %s.' % (sys.argv[idx+1], n_cores)
         sys.exit(1)
-    del sys.argv[1:3]
+    del sys.argv[idx]
+    del sys.argv[idx+1]
 
-#rundir = os.path.abspath(sys.argv[1])
+if '-n' in sys.argv:
+    idx = sys.argv.index('-n')    
+    try:
+        n_decoys = int(sys.argv[idx+1])
+    except:
+        print 'Number of decoys -n must be an integer, %r is not. Default is %s.' % (sys.argv[idx+1], n_decoys)
+        sys.exit(1)
+    del sys.argv[idx]
+    del sys.argv[idx+1]
+
 seqfile = os.path.abspath(sys.argv[1])
 ros_cfile = os.path.abspath(sys.argv[2])
 
 ### The number of decoy structures produced in a single run depends on the 
 ### of cores. In total it should be at least 2000 to get native-like structures.
 ### (see PconsFold paper)
-decoys_per_core = int(math.ceil(2000.0 / cores))
+decoys_per_core = int(math.ceil(float(n_decoys) / n_cores))
 
 
-### 1..16 folders containing run 1..16
+### 1..n_cores folders containing run 1..n_cores
 plist = []
-for core in range(1, cores + 1):
-    os.chdir(rundir)
+for core in range(1, n_cores + 1):
     optionstr = ''
     optionstr += '-in:file:fasta ' + seqfile + '\n'
     optionstr += '-in:file:frag3 ' + rundir + '/aa1xxxx03_05.200_v1_3\n'
@@ -48,6 +52,7 @@ for core in range(1, cores + 1):
     optionstr += '-database ' + rosettadir + '/rosetta_database/\n'
     optionstr += '-psipred_ss2 ' + seqfile + '.ss2/\n'
     optionstr += '-out:nstruct ' + str(decoys_per_core) + '\n'
+    optionstr += '-seed_offset ' + str(core) + '\n'
 
     call(['mkdir', 'run_' + str(core)])
     os.chdir('run_' + str(core))
@@ -56,8 +61,8 @@ for core in range(1, cores + 1):
     optionfile.write(optionstr)
     optionfile.close()
 
-    plist.append(Popen([rosettadir + '/rosetta_source/bin/AbinitioRelax.static.linuxgccrelease', '@abinitio.options']))
-
+    plist.append(Popen([rosetta_abinitiorelax, '@abinitio.options']))
+    os.chdir('..')
 
 for p in plist:
     p.wait()
