a70298bbedac7a40d8c6e151ddf28843f2c1f12f
Mirco Michel <mirco.michel@scilifelab.se>
2014-03-26 15:42:07 +0100

    Update Rosetta input preparation.

--- folding/rosetta/prepare_input.py
+++ folding/rosetta/prepare_input.py
@@ -2,30 +2,28 @@ import sys
 import os
 from subprocess import call
 
-from reformat_contacts import reformat
+import reformat_contacts
 from localconfig import *
 
 if __name__ == '__main__':
-
-    os.chdir(rootdir)
 
     seqfile = sys.argv[1]
     cfile = sys.argv[2]
     factor = sys.argv[3]
 
-    call(['mkdir', rundir])
+    if not os.path.exists('rosetta'):
+        call(['mkdir', 'rosetta'])
 
     ### reformat the contact maps into constraint files used by rosetta
     ### creates: "cfile-<factor>.constraints"
-    reformat(seqfile, cfile, factor)
-    ros_cfile = cfile[:-4] + '-' + str(factor) + '.constraints'
-    call(['mv', ros_cfile, rundir])
+    ros_cfile = cfile + '-' + str(factor) + '.constraints'
+    
+    reformat_contacts.reformat(seqfile, cfile, factor, outfile_name=ros_cfile)
+    call(['mv', ros_cfile, 'rosetta'])
 
     ### run make_fragments.pl to create fragment database
     ### NOTICE: paths in make_fragments.pl need to be adjusted
-    call(['cp', seqfile, rundir])
-    call(['cp', seqfile + '.ss2', rundir])
-    os.chdir(rundir)
-    call([rosettadir + '/rosetta_fragments/make_fragments.pl', seqfile, '-nosam', '-noporter', '-psipredfile', seqfile + '.ss2', '-id', '1xxxx'])
-
-
+    call(['cp', seqfile, 'rosetta'])
+    os.chdir('rosetta')
+    call([rosetta_make_fragments, seqfile, '-nosam', '-noporter'])
+    os.chdir('..')
